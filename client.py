82251b7300b7ab3c1db21dfe928368154ed1609c
ofekg <ofekgitmul@gmail.com>
2024-12-31 16:13:59 +0200

    created client

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+tcp_socket = socket.create_connection(('localhost', 81))
+
+try:
+    data = str.encode("Hi. I am a TCP client sending data to the server")
+    tcp_socket.sendall(data)
+
+finally:
+    print("Closing socket")
+    tcp_socket.close()
