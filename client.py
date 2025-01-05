3c4dfe352f66054b4911e2b1a63430399d74f1f6
ofekg <ofekgitmul@gmail.com>
2025-01-05 14:01:56 +0200

    created client

--- client.py
+++ client.py
@@ -1,37 +1,44 @@
 import socket
 import CONSTS
 
+def divide_message(message, max_size):
+    """Divide the message into chunks of max_size."""
+    return [message[i:i+max_size] for i in range(0, len(message), max_size)]
 
-def divideMessage():
-    print("hello")
+server_address = ('localhost', 81)
 
+# Connect to the server
+with socket.create_connection(server_address) as tcp_socket:
+    try:
+        tcp_socket.sendall("Max size".encode())
+        max_size = int(tcp_socket.recv(CONSTS.BUFFER_SIZE).decode())
+        print(f"Server buffer size: {max_size}")
 
-tcp_socket = socket.create_connection(('localhost', 81))
-
-try:
-    tcp_socket.sendall(str.encode("Max size"))
-    maxSize = tcp_socket.recv(CONSTS.BUFFER_SIZE).decode()
-    print(maxSize)
-finally:
-    while True:
-        data = input()
-        try:
-
+        while True:
+            data = input("Enter message (type 'exit' to close): ")
 
             if data == "exit":
+                tcp_socket.sendall(data.encode())
+                print("Exiting client...")
                 break
 
             try:
-                encodedData = data.encode("utf-8")
-                if len(encodedData) <= int(maxSize):
-                    tcp_socket.sendall(encodedData)
+                encoded_data = data.encode("utf-8")
+                if len(encoded_data) <= max_size:
+                    tcp_socket.sendall(encoded_data)
                 else:
-                    divideMessage()
+                    chunks = divide_message(data, max_size)
+                    for chunk in chunks:
+                        tcp_socket.sendall(chunk.encode("utf-8"))
 
                 response = tcp_socket.recv(CONSTS.BUFFER_SIZE).decode()
-                print(response)
-            finally:
-                print("hey")
+                print(f"Response from server: {response}")
 
-        finally:
-            print("closed")
+            except Exception as e:
+                print(f"Error during communication: {e}")
+
+    except Exception as e:
+        print(f"Error connecting to server: {e}")
+
+    finally:
+        print("Connection closed.")
--- server.py
+++ server.py
@@ -1,32 +1,40 @@
+### server.py
 import socket
-
 import CONSTS
-
 
 server_address = ('localhost', 81)
 
+# Set up the server socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-
     server_socket.bind(server_address)
     server_socket.listen(1)
+    print("Server is up and running, waiting for a connection...")
 
     while True:
-        print("waiting for connection")
-        connection, client = server_socket.accept()
+        connection, client_address = server_socket.accept()
+        print(f"Connected to client IP: {client_address}")
 
         try:
-            print("connected to client IP: {}".format(client))
+            while True:
+                data = connection.recv(CONSTS.BUFFER_SIZE)
+                if not data:
+                    print("No data received. Closing connection.")
+                    break
 
-            data = connection.recv(CONSTS.BUFFER_SIZE)
+                message = data.decode("utf-8")
+                print(f"Received data: {message}")
 
-            if data.decode() == "Max size":
-                connection.sendall(str(CONSTS.BUFFER_SIZE).encode())
+                if message == "Max size":
+                    connection.sendall(str(CONSTS.BUFFER_SIZE).encode())
+                elif message == "exit":
+                    print("Client requested to close the connection.")
+                    break
+                else:
+                    response = f"Server received: {message}"
+                    connection.sendall(response.encode())
 
-            if not data:
-                break
-
-            print("Recieved data: {}".format(data))
-        except KeyboardInterrupt:
-            print("Shutting down...")
-            break
-
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            print("Closing client connection...")
+            connection.close()
